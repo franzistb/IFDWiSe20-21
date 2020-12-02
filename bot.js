ae05975fa0018e187743ee6a92032a24f24208a6
franzistb <72912912+franzistb@users.noreply.github.com>
2020-12-02 18:13:54 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,57 +4,57 @@ window.addEventListener("load", function () {
 		var artyom = new Artyom();
 		artyom.addCommands([
 			{
-				indexes: ["hallo!", "hallo!"],
+				indexes: ["guten morgen haki", "guten Morgen harki"],
 				action: function () {
-					document.getElementById('in').innerHTML ="Guten Morgen! Was kann ich für dich heute tun?";
-					artyom.say("Guten Morgen! Was kann ich für dich heute tun?");
+					document.getElementById('in').innerHTML ="Guten Morgen Marcel, was kann ich für dich tun?";
+					artyom.say("Guten Morgen Marcel, was kann ich für dich tun?");
 				}
 			},
 
 			{
-				indexes: ["was gibt es Neues?", "was gibt es Neues"],
+				indexes: ["haki bitte checke meinen HFU Webmail Account", "Haki bitte checke meine HFU Webmail Account"],
 				action: function () {
-					document.getElementById('in').innerHTML ="Was möchtest du zuerst wissen? Mails, Stundenplan, Wetter oder Essen";
-					artyom.say("Was möchtest du zuerst wissen? Mails, Stundenplan, Wetter oder Essen");
+					document.getElementById('in').innerHTML ="Du hast 4 neue Nachrichten. Möchtest du, dass ich sie vorlese?";
+					artyom.say("Du hast 4 neue Nachrichten. Möchtest du, dass ich sie vorlese?");
 				}
 			},
 			{
-				indexes: ["bitte meine Mails", "bitte meine Mails"],
+				indexes: ["bitte lese mir nur die Absender vor", "bitte lese mir nur die Abstände vor"],
 				action: function () {
-					document.getElementById('in').innerHTML ="Du hast 5 neue Mails bekommen";
-					artyom.say("Du hast 5 neue Mails bekommen");
+					document.getElementById('in').innerHTML ="Die Abesnder sind: HFU Sekretariat, Xing News, Felix.";
+					artyom.say("Die Absender sind: HFU Sekretariat, Xing News, Felix");
 				}
 			},
 			{
-				indexes: ["wie sieht mein Stundenplan am Montag aus", "wie sieht mein Stundenplan am Montag aus"],
+				indexes: ["okay danke Haki bitte lese mir die Email vom Hfu sekretariat vor", "okay danke Hacki bitte lese mir die E-Mail vom HFU sekretariat vor"],
 				action: function () {
-					document.getElementById('in').innerHTML ="Du hast zuerst Informatik. Danach Gesamtwirtschaft und danach Markteing";
-					artyom.say("Du hast zuerst Informatik. Danach Gesamtwirtschaft und danach Markteing");
+					document.getElementById('in').innerHTML ="Aber klar doch! Hier die Nachricht. Versendet am 01.12.2020. Betreff: xy. Nachricht: xy";
+					artyom.say("Aber klar doch! Hier die Nachricht. Versendet am 01.12.2020. Betreff: xy. Nachricht: xy");
 				}
 			},
 			{
-				indexes: ["was gibt es heute zum essen", "was gibt es heute zum essen"],
+				indexes: ["welche Vorlesungen habe ich heute", "welche Vorlesung habe ich heute"],
 				action: function () {
-					document.getElementById('in').innerHTML ="Nach einem Blick in den Kühlschrank würde ich dir einen Gemüseauflauf im Ofen empfehlen. Du hast noch reichlich Gemüse, dass du verwenden musst";
-					artyom.say("Nach einem Blick in den Kühlschrank würde ich dir einen Gemüseauflauf im Ofen empfehlen. Du hast noch reichlich Gemüse, dass du verwenden musst");
+					document.getElementById('in').innerHTML ="Für den Donnerstag den 08.11.2020 hast du folgende Vorlesungen: 8:45-9:30 Uhr Streaminganwendungen";
+					artyom.say("Für den Donnerstag den 08.11.2020 hast du folgende Vorlesungen: 8:45-9:30 Uhr Streaminganwendungen");
 				}
 			},
 			{
-				indexes: ["wie wird das Wetter in Furtwangen", "wie wird das Wetter in Furtwangen"],
+				indexes: ["super danke welche abgaben muss ich für diese Fächer diese Woche fertig machen"],
 				action: function () {
-					document.getElementById('in').innerHTML ="Das Wetter in Furtwangen wird morgen bewölkt sein mit 10 Grad";
-					artyom.say("Das Wetter in Furtwangen wird morgen bewölkt sein mit 10 Grad");
+					document.getElementById('in').innerHTML ="Folgende Abgaben müssen bis zum 15.11 abgegeben sein: Streaminganwendungen Aufgabe 3, Interface Design: Aufhabe 4";
+					artyom.say("Folgende Abgaben müssen bis zum 15.11 abgegeben sein: Streaminganwendungen Aufgabe 3, Interface Design: Aufhabe 4");
 				}
 			},
 			{
-				indexes: ["gibt es weitere Infos zur Uni?", "gibt es weitere Infos zur Uni?"],
+				indexes: ["wo finde ich die Aufgabenstellung für die Abgabe in Streaminganwendungen", "wo finde ich die Aufgabenstellung für die Abgabe in stream mir Anwendungen", "wo finde ich die Aufgabenstellung für die Abgabe in Streaming Anwendungen"],
 				action: function (i) {
-					document.getElementById('in').innerHTML ="Info 1: Deine Abgabe für Interface Design ist in drei Tagen fällig. Info 2: Nächste Woche fällt die Vorlesung in Streaming Anwendungen aus";
-					artyom.say("Info 1: Deine Abgabe für Interface Design ist in drei Tagen fällig. Info 2: Nächste Woche fällt die Vorlesung in Streaming Anwendungen aus");
+					document.getElementById('in').innerHTML ="Die Aufgabenstellung findest du in FELIX im Ordner Streaminganwendungen. Soll ich dir diese Aufgabe vorlesen Marcel?";
+					artyom.say("Die Aufgabenstellung findest du in FELIX im Ordner Streaminganwendungen. Soll ich dir diese Aufgabe vorlesen Marcel?");
 				}
 			},
 			{
-				indexes: ["danke für deine Hilfe"],
+				indexes: ["danke haki ich werd in FELIX nachschauen!"],
 				action: function (i) {
 					artyom.fatality();
 				}
